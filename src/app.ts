94de40bee4260edf96e748ac87ac6a17c7fb5064
Khnykin-Artem <k98030118@gmail.com>
2023-03-02 23:01:51 +0300

    server.ts

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import compression from 'compression';
+import morganMiddleware from './middlewares/morganMiddleware';
 // import router from "./routes/index";
 
 const app = express();
 
+app.use(express.json());
 app.use(compression());
-app.use();
+app.use(morganMiddleware);
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,13 @@ import { config } from 'node-config-ts';
 import http from 'node:http';
 import app from './app';
 import db from './db';
+import logger from './lib/logger';
 
 const server = http.createServer(app);
-db().then().catch();
+void db()
 
 server.listen(config.server.http.port, config.server.http.host, () => {
-  console.log(
+  logger.info(
     `Server started on http://${config.server.http.host}:${config.server.http.port}`
   );
 });
